fc25492d34e82ce984fe9e44a0555d02610d12ff
zivserphos <zivfromisrael@gmail.com>
2022-01-24 18:23:04 +0200

    progress 3.0

--- server/src/controllers/homePageStatic.ts
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import { Handler } from "express";
-
-const homePage: Handler = (_req, res) => {
-  res.sendFile(path.resolve("../client/build/index.html"));
-};
-
-export default homePage;
